9e9a2974bcf0710e24117a0ca311fa077c4eff42
MasterPrasanjit <prabeerpanda07@gmail.com>
2023-02-12 13:59:25 +0530

    Add constants file `snowflakes.py` for Discord IDs

---
+++ constants/snowflakes.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class GuildID(Enum):
+    MAIN = 1013416695425159188
+    TEST = 975681097956491275
+
+
+class ChannelID(Enum):
+    ROSTER = 1030310886465871902
+
+
+class MessageID(Enum):
+    ROSTER = 1074230046530740274
